687d8c7f7f754434ba331ad7635352779f6017ad
jjIval88 <138240438+jjIval88@users.noreply.github.com>
2023-08-21 19:00:48 +0300

    Update Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,3 +9,5 @@ module.exports = function (grunt) {
     // Load all grunt tasks from node_modules, and config from /grunt/config
     require('load-grunt-config')(grunt, {
         configPath: path.join(process.cwd(), 'grunt/config'),
+        config: {
+            pkg: grunt.file.readJSON('package.json'),
